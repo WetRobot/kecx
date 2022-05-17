8e46b55fde02f9a73578b8a658784c8945870df3
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2022-05-17 22:52:07 +0300

    init

---
+++ examples/input1.cpp
@@ -0,0 +1,9 @@
+
+// @codedoc_comment_block key1
+// @codedoc_comment_block key2
+// Comment line 1
+// Comment line 2
+// Comment line 3
+// @codedoc_comment_block key2
+// @codedoc_comment_block key1
+
